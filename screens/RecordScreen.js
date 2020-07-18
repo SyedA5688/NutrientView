37ea950ee81c3db61a5a092c355bd26cd47ccb18
Syed Rizvi <57242341+SyedA5688@users.noreply.github.com>
2020-07-18 14:03:29 -0500

    Added image storage using firebase storage and image url retrieval

--- screens/RecordScreen.js
+++ screens/RecordScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Alert } from 'react-native';
 import Header from '../components/header';
 import Loading from '../components/loading';
 import NoAccess from '../components/noAccess';
 import ApiKeys from '../constants/ApiKeys';
 import { Camera } from 'expo-camera';
-import { FontAwesome5, Ionicons } from '@expo/vector-icons'; 
+import { FontAwesome5, Ionicons } from '@expo/vector-icons';
+import base64 from 'react-native-base64';
+import { ImagePicker } from 'expo';
+import * as firebase from 'firebase';
 
 
 export default class RecordScreen extends React.Component
@@ -24,33 +27,65 @@ export default class RecordScreen extends React.Component
   }
 
   onShutterPress = async () => {
+    let photo;
     if (this.camera) {
-      let photo = await this.camera.takePictureAsync();
+      photo = await this.camera.takePictureAsync();
     }
-    // http://api.foodai.org/v1/classify?image_url=https://example.jpg&num_tag=5&api_key=[YOUR KEY HERE]
-    // Call to FoodAI image recognition API
-    const response = fetch("http://api.foodai.org/v1/classify?image_url=https://pixabay.com/photos/hamburger-burger-barbeque-bbq-beef-1238246&num_tag=5&api_key=" + ApiKeys.foodAI.apiKey)
-      .then(response => response.json())
-      .then(data => console.log(data));
+    // Upload image to firebase, get image url IBM Watson API
+    let time = Date.now().toString(); // Name of image will be the time created, in milliseconds since Jan 1, 1970, 00:00:00 UTC
+    const response = this.uploadImageToFirebase(photo.uri, time);
+  }
 
+  uploadImageToFirebase = async (fileUri, time) => {
+    let uid = firebase.auth().currentUser.uid;
+    const imageRef = firebase.storage().ref().child("users").child(uid).child("images").child(time);
+
+    // Upload Image to firebase, need to use Javascript Blob API*
+    const response = await fetch(fileUri);
+    const blob = await response.blob();
+    return imageRef.put(blob)
+      .then(() => {
+        console.log("Successful Image Upload to Firebase");
+        return imageRef.getDownloadURL();
+      })
+      .then(url => {
+        console.log("Firebase URL: " + url);
+        this.callImageRecognition(url);
+      })
+      .catch(error => {
+        Alert.alert("Error in uploading image: " + error);
+        return;
+      })
+  }
+
+  callImageRecognition = async (fileUrl) => {
+    const options = {
+      headers: {
+        Authorization: "Basic " + base64.encode("apiKey:" + ApiKeys.watsonVisualRecognition.apiKey)
+      }
+    }
+    const response = fetch("https://api.us-south.visual-recognition.watson.cloud.ibm.com/instances/4c100334-28b1-4090-b07d-7e86a67b005e/v3/classify?url=" 
+                            + encodeURIComponent(fileUrl) + "&version=2018-03-19&classifier_ids=food", options)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        Alert.alert("Error in uploading image: " + error);
+        return;
+      })
+  }
+
+  onImagePickPress = async () => {
+    // let result = await ImagePicker.launchImageLibraryAsync();
   }
 
   render(){
     if (this.state.hasPermission === null) {
-      return (
-        <View>
-          <Header />
-          <Loading />
-        </View>
-      )
+      return (<View><Header /><Loading /></View>)
     }
     if (this.state.hasPermission === false) {
-      return (
-        <View>
-          <Header />
-          <NoAccess />
-        </View>
-      )
+      return (<View><Header /><NoAccess /></View>)
     }
     return (
       <View style={styles.container} >
@@ -60,6 +95,9 @@ export default class RecordScreen extends React.Component
           type={this.state.type} 
           ref={ref => { this.camera = ref; }}
         >
+          <View style={styles.cameraCaptionBox} >
+            <Text style={styles.cameraCaptionText} >Please allow several seconds for image processing</Text>
+          </View>
         </Camera>
 
         {/* Controls */}
@@ -93,6 +131,16 @@ const styles = StyleSheet.create({
   camera: {
     width: '100%',
     height: (Dimensions.get('window').height * 2) / 3,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  cameraCaptionBox: {
+    backgroundColor: 'transparent',
+    paddingVertical: 1,
+  },
+  cameraCaptionText: {
+    color: 'white',
+    textAlign: 'center',
   },
   cameraControlsContainer: {
     width: '100%',
